8b0af29ad5aa20865351f23361eae6e21682979c
davi <fs.oliveira0014@gmail.com>
2024-10-04 17:28:27 -0300

    por hj deu

--- src/pages/usuarios/index.jsx
+++ src/pages/usuarios/index.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-
 export default function Usuarios() {
 
     return (
