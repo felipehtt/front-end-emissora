509dc0c976927bd3704d5b929507ab3dbfbc5a6e
davi <daviforever17@gmail.com>
2024-10-04 17:14:07 -0300

    d.

---
+++ src/pages/usuarios/index.jsx
@@ -0,0 +1,36 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function Usuarios() {
+
+    return (
+
+        <div className='pagina-home'>
+
+            <div>
+
+            <div className='top'>
+                <a className='icon' href="/"><FontAwesomeIcon icon={faArrowLeft} size='2x' color='#000' /></a>
+
+                <h1>USUARIOS</h1>
+            </div>
+
+                <h2>Usuarios que assistem a Tv</h2>
+
+                <img src="" alt="" />
+
+                <ul>
+                    <li><Link to='/consultar'>Consultar Usuarios</Link></li>
+                    <li><Link to='/cadastrar'>Cadastrar Usuarios</Link></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    );
+
+}
